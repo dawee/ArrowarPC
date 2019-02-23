bf0a1b6adda09ce451e80ca1431faedc19a1b10c
Estelle Martinez <rikku.shimizu@gmail.com>
2019-02-23 14:03:55 -0600

    Players prefabs

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    public enum Type {
+        Heart,
+        Shield,
+        Bomb
+    };
+
+    [SerializeField]
+    private Type type;
+}
